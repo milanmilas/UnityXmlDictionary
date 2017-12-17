7d97d0f24641810f959a5c0ab01569fbc0c84f76
Milan Milas <milanmilas@gmail.com>
2017-12-17 22:36:16 +0000

    initial

---
+++ UnityDictionaryInstance/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnityDictionaryInstance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IUnityContainer container = new UnityContainer();
+            container.LoadConfiguration();
+
+            var store = container.Resolve<ITenantStore>();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ UnityDictionaryInstance/TenantStore.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnityDictionaryInstance
+{
+    public class TenantStore : ITenantStore
+    {
+        public TenantStore(Dictionary<string, string> dictionary)
+        {
+                
+        }
+
+        public IEnumerable<string> GetTenantNames()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UploadLogo(string tenant, byte[] logo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
